4550759f0faf6e2aaf0673ea0c8d9ae2dced669c
KatieJane07 <150103698+KatieJane07@users.noreply.github.com>
2024-09-19 10:51:01 -0600

    added first demo :penguin:

---
+++ index.md
@@ -0,0 +1,7 @@
+# Comp Sci 30
+
+## Demos
+- [Square Controls](squire)
+
+
+## Projects
---
+++ squire/sketch.js
@@ -0,0 +1,34 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+let x = 209
+let y = 100
+function setup() {
+  createCanvas(400, 400);
+  
+}
+
+function draw() {
+  background(220);
+  
+    if (keyIsDown(39)===true){
+      x +=2;
+    }
+    if (keyIsDown(38)===true){
+      y -=2;
+    }
+    if (keyIsDown(37)===true){
+      x -=2;
+    }
+    if (keyIsDown(40)===true){
+      y +=2;
+    }
+    fill(124,223,197)
+  rect(x,y,40)
+  
+}
