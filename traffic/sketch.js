57d598a8a072638fd87f376919382bb5b06ad29e
KatieJane07 <150103698+KatieJane07@users.noreply.github.com>
2024-09-25 11:00:08 -0600

    fixed :vertical_traffic_light:

--- traffic/sketch.js
+++ traffic/sketch.js
@@ -1,19 +1,18 @@
 // Traffic Light Starter Code
-// Your Name Here
-// The Date Here
+// Katie Strawson
+// Sept 24
 
-// GOAL: make a 'traffic light' simulator. For now, just have the light
-// changing according to time. You may want to investigate the millis()
-// function at https://p5js.org/reference/#/p5/millis
+// GOAL: make a 'traffic light' simulator. 
+
 let state;
-let red;
+let pink;
 let orange;
-let green;
+let go;
 let lastswitch = 0;
-let sometime = 2000;
+
 function setup() {
   createCanvas(600, 600);
-  state = 1
+  state = 1;
 }
 
 function draw() {
@@ -22,58 +21,58 @@ function draw() {
 
   //red
   if (state === 1){
-    if (millis()>lastswitch+2000){
-      state = 3
-      console.log (state)
-      lastswitch = millis() 
+    if (millis()>lastswitch+1000){
+      state = 3;
+      console.log (state);
+      lastswitch = millis() ;
+    }
   }
-}
 
   //orange
   if (state === 2){
-    if (millis()>lastswitch+1000){
-      state = 1
-      console.log (state)
-      lastswitch = millis()
+    if (millis()>lastswitch+500){
+      state = 1;
+      console.log (state);
+      lastswitch = millis();
+    }
   }
-}
 
   //green
   if (state === 3){
-    if (millis()>lastswitch+3000){
-      state = 2
-      console.log (state)
-      lastswitch = millis()
+    if (millis()>lastswitch+1500){
+      state = 2;
+      console.log (state);
+      lastswitch = millis();
+    }
   }
-}
 }
 
 function drawOutlineOfLights() {
   //box
   if (state === 1){
-    red = "red"
-    orange = "white"
-    green = "white"
+    pink = "red";
+    orange = "white";
+    go = "white";
   }
   if (state === 2){
-    red = "white"
-    orange = "orange"
-    green = "white"
+    pink = "white";
+    orange = "orange";
+    go = "white";
   }
   if (state === 3){
-    red = "white"
-    orange = "white"
-    green = "green"
+    pink = "white";
+    orange = "white";
+    go = "green";
   }
   rectMode(CENTER);
   fill(0);
   rect(width/2, height/2, 75, 200, 10);
   
   //lights
-  fill(red);
+  fill(pink);
   ellipse(width/2, height/2 - 65, 50, 50); //top
-  fill(orange)
+  fill(orange);
   ellipse(width/2, height/2, 50, 50); //middle
-  fill(green)
+  fill(go);
   ellipse(width/2, height/2 + 65, 50, 50); //bottom
 }
