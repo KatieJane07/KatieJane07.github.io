546883e21ed545261f9811d187aea2fd224fdec7
KatieJane07 <150103698+KatieJane07@users.noreply.github.com>
2024-10-09 11:00:35 -0600

    tested kill methods

--- object-notation-arrays/sketch.js
+++ object-notation-arrays/sketch.js
@@ -5,6 +5,10 @@
 let circleX;
 let circleY;
 let time = 1;
+let flies = [];
+let killable;
+let circles = true;
+let triangles = false;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -17,11 +21,23 @@ function draw() {
 
   circleX = noise(time) * width;
   circleY = noise(time + 3) * height;
-  time+=0.01;
+  time+=0.005;
 
-  circle(circleX,circleY, 50);
-
-  if (mouseIsPressed && (mouseY <= circleY + 50 || mouseY >= circleY - 50) && (mouseX <= circleX + 50 || mouseX >= circleX - 50) ) {
-    circle(100,100,100);
+  if (circles === true){
+    circle(circleX,circleY, 50, killable = true);
   }
+
+  if (mouseIsPressed && (mouseY <= circleY + 50 && mouseY >= circleY - 50) && (mouseX <= circleX + 50 && mouseX >= circleX - 50) ) {
+    if (killable === true){
+      dead();
+    }
+  }
+}
+
+function dead(){
+  triangles = true;
+  if (triangles === true){
+    triangle(circleX, circleY+50,circleX-50, circleY-50, circleX+50,circleY-50);
+  }
+  circles = false;
 }
